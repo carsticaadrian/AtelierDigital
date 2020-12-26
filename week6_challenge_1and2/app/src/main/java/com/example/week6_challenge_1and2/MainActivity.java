9138f94f58dfbb276a2147b50f3a0e9f019e90be
Carstica Adrian <carsticaadrian@gmail.com>
2020-12-26 19:48:13 +0200

    Week6 Challenge 1 and 2

---
+++ week6_challenge_1and2/app/src/main/java/com/example/week6_challenge_1and2/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.week6_challenge_1and2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentTransaction ft=getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.placeHolder,new SecondFrament());
+        ft.commit();
+    }
+}
