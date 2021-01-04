b6302c9424d324c165a94da36a58e585a76c7d97
Carstica Adrian <carsticaadrian@gmail.com>
2021-01-04 18:59:17 +0200

    Week6 Challenge 7

---
+++ week6_challenge_7/app/src/main/java/com/example/week6_challenge_7/ui/main/PageViewModel.java
@@ -0,0 +1,35 @@
+package com.example.week6_challenge_7.ui.main;
+
+import androidx.arch.core.util.Function;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+import androidx.lifecycle.ViewModel;
+
+public class PageViewModel extends ViewModel {
+
+    private MutableLiveData<Integer> mIndex = new MutableLiveData<>();
+    private LiveData<String> mText = Transformations.map(mIndex, new Function<Integer, String>() {
+        @Override
+        public String apply(Integer input) {
+            if(input==1){
+                return "12 Angry Men";
+            }
+            if(input==2){
+                return "Pet";
+            }
+            if(input==3){
+                return "Burger";
+            }
+            return "";
+        }
+    });
+
+    public void setIndex(int index) {
+        mIndex.setValue(index);
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ week6_challenge_7/app/src/main/java/com/example/week6_challenge_7/ui/main/PlaceholderFragment.java
@@ -0,0 +1,73 @@
+package com.example.week6_challenge_7.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.week6_challenge_7.MainActivity;
+import com.example.week6_challenge_7.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PlaceholderFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private PageViewModel pageViewModel;
+
+    public static PlaceholderFragment newInstance(int index) {
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageViewModel = new ViewModelProvider(this).get(PageViewModel.class);
+        int index = 1;
+        if (getArguments() != null) {
+            index = getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+        pageViewModel.setIndex(index);
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        final TextView textView = root.findViewById(R.id.section_label);
+        final ImageView imgView=root.findViewById(R.id.section_picture);
+        pageViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                if(s.equals("12 Angry Men")){
+                    textView.setText(s);
+                    imgView.setImageResource(R.drawable.movie);
+                }
+                if(s.equals("Pet")){
+                    imgView.setImageResource(R.drawable.cat);
+                }
+                if(s.equals("Burger")){
+                    textView.setText(s);
+                    imgView.setImageResource(R.drawable.burger);
+                }
+            }
+        });
+        return root;
+    }
+}
